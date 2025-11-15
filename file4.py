6a0084a7022a125ad0727066bcc2a2ca5eb0c452
shanmukha123307 <anusha.palgot@gmail.com>
2025-11-15 20:35:42 +0530

    Add files via upload

---
+++ file4.py
@@ -0,0 +1,12 @@
+num = int(input("Enter a number: "))
+count = 0
+if num == 0:
+    count = 1
+else:
+    num = abs(num)
+    
+    while num > 0:
+        num //= 10
+        count += 1
+
+print("Number of digits:", count)
